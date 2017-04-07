aa87a31fb745958b67f9575ee0731131014f429f
Vadim <vadjv@ya.ru>
2017-04-07 11:48:32 +0300

    Add new style 07-04-17

--- public-src/js/App.js
+++ public-src/js/App.js
@@ -35,27 +35,27 @@ class App extends Component {
             <div id="wrapper" className={(this.state.showMenu) ? "" + ( ( this.state.mapFull ) ? "mapFull" : "") : "hide " + ( ( this.state.mapFull ) ? "mapFull" : "")}>
                 <MainMenu />
                 <div className="heder" >
-                    <i className="fa fa-bars fa-2x menu_ico" onClick={::this.hideMenu} id="hide_menu"/>
+                   <i className="fa fa-bars fa-2x menu_ico" onClick={::this.hideMenu} id="hide_menu"/>
                     <a className="logo-link" href="/"><img className="logo-link_img" src="./img/Logo.svg" alt=""/></a>
                 </div>
                 <div className="content__wrap">
 
                     <div className="main">
                         <div className="block block-top block_map" >
-                            <i className="fa fa-expand fa-2x menu_ico" onClick={::this.mapFull} />
+                            <div className="item_header"><div className="map_heder_title">1111</div><i className="fa fa-expand fa-1x menu_ico ico_map_full ico_hover" onClick={::this.mapFull} /></div>
                             <Map />
                         </div>
-                        <div className="block block-bottom" />
+                        <div className="block block-bottom"><div className="item_header"><div className="map_heder_title">444</div><i className="fa fa-expand fa-1x menu_ico ico_map_full ico_hover" onClick={::this.mapFull} /></div></div>
 
                     </div>
                     <div className="aside aside-1">
-                        <div className="block block-top" />
-                        <div className="block block-bottom" />
+                        <div className="block block-top"><div className="item_header"><div className="map_heder_title">222</div></div></div>
+                        <div className="block block-bottom"><div className="item_header"><div className="map_heder_title">5555</div><i className="fa fa-expand fa-1x menu_ico ico_map_full ico_hover" onClick={::this.mapFull} /></div></div>
                     </div>
 
                     <div className="aside aside-2">
-                        <div className="block block-top" />
-                        <div className="block block-bottom" />
+                        <div className="block block-top"><div className="item_header"><div className="map_heder_title">333</div></div></div>
+                        <div className="block block-bottom"><div className="item_header"><div className="map_heder_title">666</div></div></div>
                     </div>
 
                 </div>
