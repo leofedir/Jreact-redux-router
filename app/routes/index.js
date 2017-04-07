215217def32d91559fe0530b8c85153362213dab
Serhiy <popadukserhiy@gmail.com>
2017-04-07 17:56:54 +0300

    load table list

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router(),
       bodyParser = require('body-parser'),
-
+      pgdb = require('../libs/pgdb')(),
       demografiya = require('./demografiya'),
       GeoJson = require( '../libs/createGeoJson');
 
@@ -12,6 +12,16 @@ router.use(bodyParser.urlencoded({
 
 router.post('/main', function(req, res) {
     GeoJson.queryBase(req.originalUrl, 'borders', res);
+});
+
+router.post('/gettable', function(req, res) {
+    pgdb.query(`select table_name from enter.INFORMATION_SCHEMA.TABLES where table_name like 'ludina%'`)
+        .then((d)=>{
+            res.json({'state' : true , data: d});
+        })
+        .catch((e)=>{
+            console.log("Error" , e);
+        })
 });
 
 demografiya(router);
--- public-src/js/getMapArea.js
+++ public-src/js/getMapArea.js
@@ -3,8 +3,28 @@ import { Lmap, ukraine } from "./PageElement/Map";
 import { checkStatus, parseJSON} from './checkJSON';
 
 let choroplethLayer;
+let tableStore = {};
+let dataStore = {};
 
 export default function getMap(item) {
+    let url = item.target.dataset.url
+    if(tableStore[url]) {
+
+    } else {
+        fetch('/gettable', {
+            method: 'post',
+            headers: {
+                "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+            },
+            body: 'foo=bar&lorem=ipsum'
+        })
+            .then(checkStatus)
+            .then(parseJSON)
+            .then(data => {
+                tableStore[url] = data
+            })
+    }
+
     Lmap.removeLayer(ukraine);
 
     let loader = document.getElementById('loader')
@@ -12,6 +32,7 @@ export default function getMap(item) {
     loader.classList.toggle("show")
 
     Lmap.once('layeradd', () => {
+        console.log(111)
         loader.classList.toggle("show")
     })
 
@@ -21,60 +42,72 @@ export default function getMap(item) {
 
     let property;
 
-    fetch(item.target.dataset.url, {
-        method: 'post',
-        headers: {
-            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-        },
-        body: 'foo=bar&lorem=ipsum'
-    })
-        .then(checkStatus)
-        .then(parseJSON)
-        .then(data => {
-            let filds =  data.data[0].properties;
-            let propertys = []
-
-            // select field width data
-            for (let key in filds) {
-                if (filds.hasOwnProperty(key) && key.slice(0, 4) === 'year') {
-                    propertys.push(key)
-                }
-            }
-
-            // create range slider title
-            let title_slider_range = []
-          
-            propertys.forEach((item) => {
-                title_slider_range.push(`<p>20${item.substring(5)}</p>`)
+    if (!dataStore[url]) {
+        fetch(url, {
+            method: 'post',
+            headers: {
+                "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+            },
+            body: 'foo=bar&lorem=ipsum'
+        })
+            .then(checkStatus)
+            .then(parseJSON)
+            .then(data => {
+                dataStore[url] = data;
+                createMap(data);
             });
+    } else {
+        createMap(dataStore[url])
+    }
 
-            console.log('title_slider_range >>', title_slider_range)
+    function createMap(data) {
 
-            let slider = document.getElementById('slider')
-            slider.innerHTML = `<div class="slider_title">${title_slider_range.join('')}</div>
+        let filds =  data.data[0].properties;
+        let propertys = []
+
+        // select field width data
+        for (let key in filds) {
+            if (filds.hasOwnProperty(key) && key.slice(0, 4) === 'year') {
+                propertys.push(key)
+            }
+        }
+
+        // create range slider title
+        let title_slider_range = []
+
+        propertys.forEach((item) => {
+            title_slider_range.push(`<p>20${item.substring(5)}</p>`)
+        });
+
+        let slider = document.getElementById('slider')
+        slider.innerHTML = `<div class="slider_title">${title_slider_range.join('')}</div>
                                 <input id='range' class="slider_range" type="range" min="0" max="${propertys.length - 1}" value="0" id="fader" step="1">`;
 
-            let range = document.getElementById('range');
-            range.addEventListener('change', function() {
-                let field = fieldNames.items[this.value].value;
-                myItem = field;
-                fieldName = field;
-                saveData(layerDataHistogram[parametr], myItem)
-                updateAttribute(field, infoTemplate)
+        let range = document.getElementById('range');
 
-                // if (myItem != 'year_13') {
-                //     mylayer3 = new ArcGISDynamicMapServiceLayer(tot, {
-                //         className: 'ato',
-                //         imageTransparency: false,
-                //         id: "ato"
-                //     });
-                //     map.addLayer(mylayer3)
-                // } else {
-                //     map.removeLayer(map.getLayer('ato'))
-                // }
-            })
+        range.addEventListener('change', function() {
+            property = propertys[this.value];
 
-            property = propertys[0]
+            renderLayer();
+
+            // if (myItem != 'year_13') {
+            //     mylayer3 = new ArcGISDynamicMapServiceLayer(tot, {
+            //         className: 'ato',
+            //         imageTransparency: false,
+            //         id: "ato"
+            //     });
+            //     map.addLayer(mylayer3)
+            // } else {
+            //     map.removeLayer(map.getLayer('ato'))
+            // }
+        })
+
+        property = propertys[0]
+
+        function renderLayer() {
+            if (Lmap.hasLayer(choroplethLayer)) {
+                Lmap.removeLayer(choroplethLayer)
+            }
 
             choroplethLayer = L.choropleth(data.data, {
                 valueProperty: property,
@@ -90,22 +123,27 @@ export default function getMap(item) {
                     layer.bindPopup(feature.properties.name_ua + "   " + feature.properties.year_13 )
                 }
             }).addTo(Lmap)
+        }
 
-            // Add legend (don't forget to add the CSS from index.html)
-                var div = document.getElementById('legend')
-                var limits = choroplethLayer.options.limits
-                var colors = choroplethLayer.options.colors
-                var labels = []
-            div.innerHTML = `<h5 className="legend__title">Назва показника</h5>`
-            let dani = 'Дані відсутні'
+        renderLayer();
 
-                limits.forEach(function (limit, i) {
-                    labels.push('<i style="background:' + colors[i] + '"></i> ' +
-                        ((limits[i] !== null) ? limits[i] : dani) + ((i !== limits.length -1 && limits[i + 1] !== null) ? ' &ndash; ' + limits[i + 1] + '<br>' : (limits[i ] !== null) ? ' +<br/>' : '<br/>'))
-                })
 
-                div.innerHTML += labels.join('')
-                return div
+        // Add legend (don't forget to add the CSS from index.html)
+        var div = document.getElementById('legend')
+        var limits = choroplethLayer.options.limits
+        var colors = choroplethLayer.options.colors
+        var labels = []
+        div.innerHTML = `<h5 className="legend__title">Назва показника</h5>`
+        let dani = 'Дані відсутні'
 
-        });
+        limits.forEach(function (limit, i) {
+            labels.push('<i style="background:' + colors[i] + '"></i> ' +
+                ((limits[i] !== null) ? limits[i] : dani) + ((i !== limits.length -1 && limits[i + 1] !== null) ? ' &ndash; ' + limits[i + 1] + '<br>' : (limits[i ] !== null) ? ' +<br/>' : '<br/>'))
+        })
+
+        div.innerHTML += labels.join('')
+        return div
+
+    }
+
 }
