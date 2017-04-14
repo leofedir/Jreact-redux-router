3751c801a997ea2b2d1551f5afcb5c9998303eaa
Serhiy <popadukserhiy@gmail.com>
2017-04-14 15:07:40 +0300

    add MEnu

---
+++ public-src/js/PageElement/Menu.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+import { menu } from './menu_src'
+
+class Menu extends Component {
+
+    getItem(items) {
+        return items.map(item => {
+            return (
+                <li className="menu__item" key={item.key}>
+                    <a href="#" data-url={item.url}><img className="menu__icon" src={'img/menu/' + item.icon}/><span className="menu__item-text">{item.name}</span></a>
+                </li>)
+        });
+    }
+
+    render() {
+        return (
+            <div id="menu_wrapper" className="menu_wrapper">
+                <div className={`icons-menu`} id="menu">
+                    <div className="menu">
+                        <ul className="menu__items">
+                            {this.getItem(menu)}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Menu
