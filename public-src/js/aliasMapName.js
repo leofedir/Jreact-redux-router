10f3ac8e6a9ac9a125a2807027296d98487b8627
Vadym <vadymas31@gmail.com>
2017-05-10 16:34:40 +0300

    aliasss

--- public-src/js/aliasMapName.js
+++ public-src/js/aliasMapName.js
@@ -8,7 +8,7 @@ export let alias = {
     area_sotspol_vyplachenipensii: 'Загальний обсяг виплачених пенсій',
     area_budgets_dohodymistsevogobudget: 'Доходи місцевих бюджетів',
     area_budgets_pdfonadushu: 'ПДФО на душу населення',
-    area_budgets_tranfertynadushu: 'Трансферти на душу населення',
+    area_budgets_ttranfertynadushu: 'Трансферти на душу населення',
     area_budgets_zempod: 'Земельний податок та плата за оренду землі на 1 га загальної земельної площі',
     area_budgets_rivpodnavnadushu: 'Рівень податкового навантаження на душу населення',
     area_budgets_rivfinzalejnosti: 'Рівень фінансової залежності місцевого бюджету',
