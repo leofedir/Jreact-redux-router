6a51a1ec9df2208458a694738900ead9dfa6adba
Serhiy <popadukserhiy@gmail.com>
2017-05-24 17:39:22 +0300

    don't work synchronize select

--- public-src/js/PageElement/BarChart.js
+++ public-src/js/PageElement/BarChart.js
@@ -220,156 +220,177 @@ class BarChart extends Component {
             });
 
         }
-        else if (chart3 !== null) {
-
-
-            // $('#container').bind('mousemove touchmove touchstart', function (e) {
-            //     var chart,
-            //         point,
-            //         i,
-            //         event;
-            //
-            //     for (i = 0; i < Highcharts.charts.length; i = i + 1) {
-            //         chart = Highcharts.charts[i];
-            //         event = chart.pointer.normalize(e.originalEvent); // Find coordinates within the chart
-            //         point = chart.series[0].searchPoint(event, true); // Get the hovered point
-            //
-            //         if (point) {
-            //             point.highlight(e);
-            //         }
-            //     }
-            // });
-            /**
-             * Override the reset function, we don't need to hide the tooltips and crosshairs.
-             */
-            Highcharts.Pointer.prototype.reset = function () {
-                return undefined;
-            };
-
-            /**
-             * Highlight a point by showing tooltip, setting hover state and draw crosshair
-             */
-            Highcharts.Point.prototype.highlight = function (event) {
-                this.onMouseOver(); // Show the hover marker
-                this.series.chart.tooltip.refresh(this); // Show the tooltip
-                this.series.chart.xAxis[0].drawCrosshair(event, this); // Show the crosshair
-            };
-
-            /**
-             * Synchronize zooming through the setExtremes event handler.
-             */
-            function syncExtremes(e) {
-                var thisChart = this.chart;
-
-                if (e.trigger !== 'syncExtremes') { // Prevent feedback loop
-                    Highcharts.each(Highcharts.charts, function (chart) {
-                        if (chart !== thisChart) {
-                            if (chart.xAxis[0].setExtremes) { // It is null while updating
-                                chart.xAxis[0].setExtremes(e.min, e.max, undefined, false, {trigger: 'syncExtremes'});
-                            }
-                        }
-                    });
-                }
-            }
-
-            let chartData = [];
-            let chartType = ['line', 'area', 'area']
-
-
-            for (let key in chart3) {
-
-                if (chart3.hasOwnProperty(key)) {
-                    let obj = {
-                        name: key,
-                        data: []
-                    };
-                    chart3[key].forEach(item => {
-                        obj.data.push(item.value)
-                    })
-
-                    chartData.push(obj)
-                }
-            }
-
-            let {chart0, chart1, chart2} = this.refs;
-
-
-            chartData.forEach((dataset, i) => {
-
-                    ['chart' + i].highcharts({
-                        chart: {
-                            marginLeft: 40, // Keep all charts left aligned
-                            spacingTop: 20,
-                            spacingBottom: 20
-                        },
-                        title: {
-                            text: dataset.name,
-                            align: 'left',
-                            margin: 0,
-                            x: 30
-                        },
-                        credits: {
-                            enabled: false
-                        },
-                        legend: {
-                            enabled: false
-                        },
-                        xAxis: {
-                            crosshair: true,
-                            events: {
-                                setExtremes: syncExtremes
-                            },
-                            labels: {
-                                format: '{value} km'
-                            }
-                        },
-                        yAxis: {
-                            title: {
-                                text: null
-                            }
-                        },
-                        tooltip: {
-                            positioner: function () {
-                                return {
-                                    x: this.chart.chartWidth - this.label.width, // right aligned
-                                    y: -1 // align to title
-                                };
-                            },
-                            borderWidth: 0,
-                            backgroundColor: 'none',
-                            pointFormat: '{point.y}',
-                            headerFormat: '',
-                            shadow: false,
-                            style: {
-                                fontSize: '18px'
-                            },
-                            valueDecimals: dataset.valueDecimals
-                        },
-                        series: [{
-                            data: dataset.data,
-                            name: dataset.name,
-                            type: chartType[i],
-                            color: Highcharts.getOptions().colors[i],
-                            fillOpacity: 0.3
-                            // tooltip: {
-                            //     valueSuffix: ' ' + dataset.unit
-                            // }
-                        }]
-                    });
-                }
-            )
-        }
         else if (!data_success && myChart !== null && chart3 === null) {
             myChart.destroy();
             myChart = null
         }
     }
 
-    componentDidMount() {
+    handlerOnMouseMuve(e) {
+        console.log('e >>', e)
+        console.log('e >>', e.type)
+        var chart,
+            point,
+            i,
+            event;
+
+
+        for (i = 0; i < Highcharts.charts.length; i++) {
+            if (Highcharts[i] !== undefined) {
+                chart = Highcharts.charts[i];
+                event = chart.pointer.normalize(e.originalEvent); // Find coordinates within the chart
+                point = chart.series[0].searchPoint(event, true); // Get the hovered point
+
+                if (point) {
+                    point.highlight(e);
+                }
+            }
+
+
+        }
+        // this.refs.multiChart.on('mousemove', function (e) {
+        //
+        // });
+
+
+    }
+
+    getMultiChart() {
+        /**
+         * Override the reset function, we don't need to hide the tooltips and crosshairs.
+         */
+        Highcharts.Pointer.prototype.reset = function () {
+            return undefined;
+        };
+
+        /**
+         * Highlight a point by showing tooltip, setting hover state and draw crosshair
+         */
+        Highcharts.Point.prototype.highlight = function (event) {
+            console.log('44444 >>', 44444)
+            this.onMouseOver(); // Show the hover marker
+            this.series.chart.tooltip.refresh(this); // Show the tooltip
+            this.series.chart.xAxis[0].drawCrosshair(event, this); // Show the crosshair
+        };
+
+        /**
+         * Synchronize zooming through the setExtremes event handler.
+         */
+
+        function syncExtremes(e) {
+            var thisChart = this.chart;
+            console.log('syncExtremes  e >>', e)
+
+            if (e.trigger !== 'syncExtremes') { // Prevent feedback loop
+                console.log('5555 >>', 5555)
+                Highcharts.each(Highcharts.charts, function (chart) {
+                    if (chart !== thisChart && chart !== undefined) {
+                        if (chart.xAxis[0].setExtremes) { // It is null while updating
+                            chart.xAxis[0].setExtremes(e.min, e.max, undefined, false, {trigger: 'syncExtremes'});
+                        }
+                    }
+                });
+            }
+        }
+
+        const {chart3} = this.props.map_reducer
+
+
+
+        let chartData = [];
+        let chartType = ['line', 'area', 'area']
+
+
+        for (let key in chart3) {
+
+            if (chart3.hasOwnProperty(key)) {
+                let obj = {
+                    name: key,
+                    data: []
+                };
+                chart3[key].forEach(item => {
+                    obj.data.push(item.value)
+                });
+
+                chartData.push(obj)
+            }
+        }
+
+        let HTML = [];
+
+        chartData.forEach((dataset, i) => {
+                HTML.push(<div key={i} id={'chart' + i}/>);
+
+                let options = new Object({
+                    chart: {
+                        marginLeft: 40, // Keep all charts left aligned
+                        spacingTop: 20,
+                        spacingBottom: 20,
+                        renderTo: 'chart' + i
+                    },
+                    title: {
+                        text: dataset.name,
+                        align: 'left',
+                        margin: 0,
+                        x: 30
+                    },
+                    credits: {
+                        enabled: false
+                    },
+                    legend: {
+                        enabled: false
+                    },
+                    xAxis: {
+                        crosshair: true,
+                        events: {
+                            setExtremes: syncExtremes
+                        },
+                        labels: {
+                            format: '{value} km'
+                        }
+                    },
+                    yAxis: {
+                        title: {
+                            text: null
+                        }
+                    },
+                    tooltip: {
+                        positioner: function () {
+                            return {
+                                x: this.chart.chartWidth - this.label.width, // right aligned
+                                y: -1 // align to title
+                            };
+                        },
+                        borderWidth: 0,
+                        backgroundColor: 'none',
+                        pointFormat: '{point.y}',
+                        headerFormat: '',
+                        shadow: false,
+                        style: {
+                            fontSize: '18px'
+                        },
+                        valueDecimals: dataset.valueDecimals
+                    },
+                    series: [{
+                        data: dataset.data,
+                        name: dataset.name,
+                        type: chartType[i],
+                        color: Highcharts.getOptions().colors[i],
+                        fillOpacity: 0.3
+                        // tooltip: {
+                        //     valueSuffix: ' ' + dataset.unit
+                        // }
+                    }]
+                })
+
+                new Highcharts.Chart(options)
+            }
+        );
     }
 
     componentDidUpdate() {
         this.createChart(this.props.map_reducer.bar_chart_full)
+        this.props.map_reducer.chart3 !== null ? this.getMultiChart() : null
     }
 
     toggleChartData() {
@@ -378,6 +399,7 @@ class BarChart extends Component {
 
     render() {
         const {bar_chart_full, chart3, dataChartRegion, data_success, properties} = this.props.map_reducer;
+        const chartDiv = <div ref="multiChart" className="multiChart" onMouseMove={::this.handlerOnMouseMuve}><div id="chart0" className="item_bar_chart"/><div id="chart1" className="item_bar_chart"/><div id="chart2" className="item_bar_chart"/></div>;
         return (
             <div className={bar_chart_full ? 'chart_2 barChart_full' : 'chart_2'}>
                 <div className="item_header">
@@ -396,10 +418,8 @@ class BarChart extends Component {
                                 <i className={ !dataChartRegion ? "fa fa-toggle-on" : 'fa fa-toggle-on fa-flip-horizontal' }/>}Райони
                         </div>
                     </div>
-                    <div id="item_bar_chart">
-                        <div ref="chart0"/>
-                        <div ref="chart1"/>
-                        <div ref="chart2"/>
+                    <div id="item_bar_chart" className="item_bar_chart">
+                        {chart3 !== null ? chartDiv : null}
                     </div>
                 </div>
             </div>
