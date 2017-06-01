033f31287483744b67046c4a2e53f204cc57b302
Sergey Lysenko <lysenko.seregei.developer@gmail.com>
2017-06-01 09:13:58 +0300

    jsx verstka slaidera

--- public-src/js/PageElement/sliderRange.js
+++ public-src/js/PageElement/sliderRange.js
@@ -5,29 +5,51 @@ class SliderRange extends Component {
         this.props.set_Range_item(e.target.value);
     }
 
+    /*
+    *  <div className="slider" id="slider">
+     <div className="slider_title">
+     {items.map((item, i) => <p key={ i }>20{item.substring(5)}</p>)}
+     </div>
+     <input
+     className="slider_range"
+     type="range"
+     min="0"
+     max={items.length - 1}
+     value={this.props.range_item    }
+     id="fader"
+     step="1"
+     onChange={ ::this.changeItem}/>
+     </div>
+    * */
     createRange() {
         const items = this.props.range_items;
-
+        const firtItem = items[0];
+        const lastItem = items[items.last-1];
+    
         return (
-            <div className="slider" id="slider">
-                <div className="slider_title">
-                    {items.map((item, i) => <p key={ i }>20{item.substring(5)}</p>)}
+            <div className="sliderRange">
+                <p className="rangeItem first">{firtItem}</p>
+                <div className="current-year-container">
+                    <div className="container-fa">
+                        <i className="fa fa-caret-left" aria-hidden="true"></i>
+                    </div>
+                
+                    <div className="current-year">
+                        <span>2013</span>
+                    </div>
+                
+                    <div className="container-fa">
+                        <i className="fa fa-caret-right" aria-hidden="true"></i>
+                    </div>
+            
                 </div>
-                <input
-                    className="slider_range"
-                    type="range"
-                    min="0"
-                    max={items.length - 1}
-                    value={this.props.range_item}
-                    id="fader"
-                    step="1"
-                    onChange={ ::this.changeItem}/>
+                <p className="rangeItem last">{lastItem}</p>
             </div>
         )
     }
 
     render() {
-        return this.props.show_range ? this.createRange() : null
+        return this.createRange();
     }
 }
 
