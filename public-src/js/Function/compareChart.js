5c5c8ec4d3804bc8a9c2a554f1d6da81093b710b
serhiy <popadukserhiy@gmail.com>
2017-07-11 19:46:00 +0300

    fix

--- public-src/js/Function/compareChart.js
+++ public-src/js/Function/compareChart.js
@@ -1,64 +1,65 @@
-let __selectPoint = [];
-let __label = null,
-    __line = null;
+let _selectPoint = {};
+let _label = null,
+    _line = null;
+
 export function clearSelectionChart() {
-    __selectPoint = [];
-    __label = null;
-    __line = null;
+    _selectPoint = {};
+    _label = null;
+    _line = null;
 }
 
 export function compareChart(e) {
 
-    if (Object.keys(__selectPoint).length < 2 && !e.point.selected) {
-        __selectPoint[e.point.x] = e.point.y;
+    if (Object.keys(_selectPoint).length < 2 && !e.point.selected) {
+        _selectPoint[e.point.x] = e.point.y;
         e.point.select(true, true);
     }
     else if (e.point.selected) {
         e.point.select(false, true);
-        delete __selectPoint[e.point.x];
+        delete _selectPoint[e.point.x];
     }
-    else if (Object.keys(__selectPoint).length === 2) {
+    else if (Object.keys(_selectPoint).length === 2) {
         e.point.select(false, false);
-        __label.destroy();
-        __line.destroy();
-        __label = __line = null;
-        __selectPoint = [];
+        _label.destroy();
+        _line.destroy();
+        _label = _line = null;
+        _selectPoint = {};
         return
     }
 
-    if (Object.keys(__selectPoint).length < 2 && __label !== null) {
-        __label.destroy();
-        __line.destroy();
-        __label = __line = null;
+    if (Object.keys(_selectPoint).length < 2 && _label !== null) {
+        _label.destroy();
+        _line.destroy();
+        _label = _line = null;
     }
 
-    if (Object.keys(__selectPoint).length == 2) {
+    if (Object.keys(_selectPoint).length == 2) {
 
-        let keys = Object.keys(__selectPoint).map(i => +i).sort((a,b) => b-a);
+        let keys = Object.keys(_selectPoint).map(i => +i).sort((a,b) => b-a);
         console.log(keys)
-        let resulr = __selectPoint[keys[0]] - __selectPoint[keys[1]];
-        let persent = (resulr / __selectPoint[keys[1]]) * 100;
+        let resulr = _selectPoint[keys[0]] - _selectPoint[keys[1]];
+        let persent = (resulr / _selectPoint[keys[1]]) * 100;
         persent = persent.toFixed(2);
 
-        __line = this.chart.addSeries({
+        _line = this.chart.addSeries({
             enableMouseTracking: false,
             dashStyle: 'Dash',
-            __lineWidth: 1,
+            _lineWidth: 1,
             showInLegend: false,
             allowPointSelect: false,
-            data__labels: false,
+            data_labels: false,
             color: '#27ae60',
             data: [
-                [+keys[1], __selectPoint[keys[1]]], [+keys[0], __selectPoint[keys[1]]],
+                [+keys[1], _selectPoint[keys[1]]], [+keys[0], _selectPoint[keys[1]]],
                 null,
-                [+keys[0], __selectPoint[keys[0]]], [+keys[0], __selectPoint[keys[1]]]
+                [+keys[0], _selectPoint[keys[0]]], [+keys[0], _selectPoint[keys[1]]]
             ]
         });
         let point = this.chart.series[1].points[1];
 
-        let labelTEXT = `<p className="__labelChart">${new Intl.NumberFormat().format(resulr)} <span>(${persent} %)</span></p> `;
+        let labelTEXT = `<p className="_labelChart">${new Intl.NumberFormat().format(resulr)} <span>(${persent} %)</span></p> `;
 
-        __label = this.chart.renderer.label(labelTEXT, point.plotX, point.plotY, 'callout', point.plotX + this.chart.plotLeft, point.plotY + this.chart.plotTop, true)
+        _label = this.chart.renderer.label(labelTEXT, point.plotX, point.plotY, 'callout', point.plotX + this.chart.plotLeft, point.plotY + this.chart.plotTop, true)
             .css({
                 color: '#FFFFFF'
             })
